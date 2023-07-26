166586bcf15773cb3ebf4497a3b5a0eb4c2d02c8
SanjeevMysore <81816168+SanjeevMysore@users.noreply.github.com>
2023-07-26 14:06:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //generate random image
 var randomnumber= Math.floor((Math.random()*7)+1); //create random number
 var randomimage='img'+randomnumber+'.jpg'; //choose the random image
-var randomimagepath='images/'+randomimage; //choose random image path
+var randomimagepath=randomimage; //choose random image path
 var image=document.querySelector('img'); // select the img query
 image.setAttribute('src', randomimagepath); //setting the path 
 
